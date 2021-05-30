d1b074018444a28773067d562d1148ad524a6ebb
junk-sound <wom2277@naver.com>
2021-05-30 17:14:50 +0900

    Title

--- src/pages/sign/sections/SignInSection.tsx
+++ src/pages/sign/sections/SignInSection.tsx
@@ -3,6 +3,7 @@ import Container from 'react-bootstrap/Container';
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
+import Pixel from "src/graphic/size/pixel";
 
 const SignInSection: React.FC = () => {
   return <Container>
@@ -12,10 +13,15 @@ const SignInSection: React.FC = () => {
 };
 
 const Title: React.FC = () => {
-  return <Container css={css({
-    backgroundColor: 'pink'
-  })}>
-    Sign In
+  return <Container>
+    <h2 css={css({
+      textAlign: 'center',
+      fontSize: new Pixel(50).value,
+      fontFamily: "ObjectSans-Regular",
+      color: '#d94d3b',
+    })}>
+      Sign In
+    </h2>
   </Container>
 };
 
--- src/pages/sign/sections/SignUpSection.tsx
+++ src/pages/sign/sections/SignUpSection.tsx
@@ -3,6 +3,7 @@ import React from "react";
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
 import Container from "react-bootstrap/Container";
+import Pixel from "src/graphic/size/pixel";
 
 const SignUpSection: React.FC = () => {
   return <Container>
@@ -12,10 +13,15 @@ const SignUpSection: React.FC = () => {
 };
 
 const Title: React.FC = () => {
-  return <Container css={css({
-    backgroundColor: 'blue'
-  })}>
-    Sign Up
+  return <Container>
+    <h2 css={css({
+      textAlign: 'center',
+      fontSize: new Pixel(50).value,
+      fontFamily: "ObjectSans-Regular",
+      color: '#d94d3b',
+    })}>
+      Sign Up
+    </h2>
   </Container>
 };
 

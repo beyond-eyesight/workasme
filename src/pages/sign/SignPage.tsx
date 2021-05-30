dbb296796ffc0ed0db2b0caa15ffdace19b17b5c
junk-sound <wom2277@naver.com>
2021-05-30 15:14:37 +0900

    define sign

---
+++ src/pages/sign/SignPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SignInSection from "src/pages/sign/sections/SignInSection";
+import SignUpSection from "src/pages/sign/sections/SignUpSection";
+
+const SignPage: React.FC = () => {
+  return <div>
+    <SignInSection />
+    <SignUpSection />
+  </div>;
+};
+
+export default SignPage;
---
+++ src/pages/sign/sections/SignUpSection.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignUpSection: React.FC = () => {
+  return <div/>;
+};
+
+export default SignUpSection;

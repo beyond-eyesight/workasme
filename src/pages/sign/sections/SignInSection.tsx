c5dfa580990884f7c9ff9fc12e91114768e12119
junk-sound <wom2277@naver.com>
2021-05-30 16:59:55 +0900

    responsive container with col, row

--- src/pages/sign/sections/SignInSection.tsx
+++ src/pages/sign/sections/SignInSection.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {css, jsx} from "@emotion/react";
 
 const SignInSection: React.FC = () => {
-  return <div/>;
+  const containerStyle = css({
+    backgroundColor: 'red',
+    boxSizing: 'content-box',
+    display: 'flex',
+    width: '40%',
+    maxWidth: 400,
+    minWidth: 300
+  });
+  return <Container css={containerStyle}>
+    <Title />
+
+  </Container>;
+};
+
+const Title: React.FC = () => {
+  return <Container css={css({
+    backgroundColor: 'pink',
+    height: 100
+  })}>
+    Sign In
+  </Container>
 };
 
 export default SignInSection;
--- src/pages/sign/sections/SignUpSection.tsx
+++ src/pages/sign/sections/SignUpSection.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import {css, jsx} from "@emotion/react";
+import Container from "react-bootstrap/Container";
 
 const SignUpSection: React.FC = () => {
-  return <div/>;
+  const containerStyle = css({
+    backgroundColor: 'black',
+    boxSizing: 'content-box',
+    display: 'flex',
+    width: '40%',
+    maxWidth: 400,
+    minWidth: 300
+  });
+  return <Container css={containerStyle}>
+    <Title />
+
+  </Container>;
+};
+
+const Title: React.FC = () => {
+  return <Container css={css({
+    backgroundColor: 'blue',
+    width: 200,
+    height: 100
+  })}>
+    Sign Up
+  </Container>
 };
 
 export default SignUpSection;

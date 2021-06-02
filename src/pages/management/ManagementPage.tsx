3c3256b461c4df9b54fabb701441c0a05a477aea
junk-sound <wom2277@naver.com>
2021-06-02 21:18:12 +0900

    removed panes

--- src/pages/management/ManagementPage.tsx
+++ src/pages/management/ManagementPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import {Col, Nav, Row, Tab, Tabs} from "react-bootstrap";
+import {Col, Row, Tab, Tabs} from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
 
 const ManagementPage: React.FC = () => {
   console.log("good Management Page");
@@ -9,26 +10,16 @@ const ManagementPage: React.FC = () => {
 };
 
 const SelectSelfOrTeamTab: React.FC = () => {
-  return <Tab.Container>
+  return <Container>
     <Col>
       <Row sm={3}>
         <Tabs defaultActiveKey="self" id="management">
           <Tab eventKey="self" title="Self"/>
-          <Tab eventKey="team" title="Team - Please Wait!" />
+          <Tab eventKey="team" title="Team - Please Wait!"/>
         </Tabs>
       </Row>
-      <Row sm={9}>
-        <Tab.Content>
-          <Tab.Pane eventKey="self">
-            <div>self section</div>
-          </Tab.Pane>
-          <Tab.Pane eventKey="team">
-            <div>team section</div>
-          </Tab.Pane>
-        </Tab.Content>
-      </Row>
     </Col>
-  </Tab.Container>
+  </Container>
 };
 
 export default ManagementPage;

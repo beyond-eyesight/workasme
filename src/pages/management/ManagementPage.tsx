48b151502573968fc572868ad921b97dbfb28c67
junk-sound <wom2277@naver.com>
2021-06-02 21:06:30 +0900

    ing - container

--- src/pages/management/ManagementPage.tsx
+++ src/pages/management/ManagementPage.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Tab, Tabs } from "react-bootstrap";
+import {Col, Nav, Row, Tab, Tabs} from "react-bootstrap";
 
 const ManagementPage: React.FC = () => {
   console.log("good Management Page");
@@ -9,14 +9,30 @@ const ManagementPage: React.FC = () => {
 };
 
 const SelectSelfOrTeamTab: React.FC = () => {
-  return <Tabs defaultActiveKey="self" id="management">
-    <Tab eventKey="self" title="Self">
-      <div>self section</div>
-    </Tab>
-    <Tab eventKey="team" title="Team - Please Wait!" disabled>
-      <div>self section</div>
-    </Tab>
-  </Tabs>
+  return <Tab.Container defaultActiveKey="self" id="manage">
+    <Col>
+      <Row sm={3}>
+        <Nav variant="pills" className="flex-row">
+          <Nav.Item>
+            <Nav.Link eventKey="self">Self</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey="team">Team</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </Row>
+      <Row sm={9}>
+        <Tab.Content>
+          <Tab.Pane eventKey="self">
+            <div>self section</div>
+          </Tab.Pane>
+          <Tab.Pane eventKey="team">
+            <div>team section</div>
+          </Tab.Pane>
+        </Tab.Content>
+      </Row>
+    </Col>
+  </Tab.Container>
 };
 
 export default ManagementPage;

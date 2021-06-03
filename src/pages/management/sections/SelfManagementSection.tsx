575b7324689ec2e63e1b6d6cd6c4a5d2d3e1e2e9
junk-sound <wom2277@naver.com>
2021-06-03 18:23:00 +0900

    layout - sidebar

--- src/pages/management/sections/SelfManagementSection.tsx
+++ src/pages/management/sections/SelfManagementSection.tsx
@@ -3,14 +3,18 @@ import YouShouldFocusOnPart from "src/pages/management/sections/parts/YouShouldF
 import EvaluationPart from "src/pages/management/sections/parts/EvaluationPart";
 import ToDoListPart from "src/pages/management/sections/parts/ToDoListPart";
 import TimeTrackerPart from "src/pages/management/sections/parts/TimeTrackerPart";
+import SideBar from "src/pages/components/bars/navigation/SideBar";
 
 const SelfManagementSection: React.FC = () => {
   return <div>
+    <SideBar />
     <YouShouldFocusOnPart />
     <TimeTrackerPart />
     <ToDoListPart />
     <EvaluationPart/>
   </div>;
 };
+
+
 
 export default SelfManagementSection;

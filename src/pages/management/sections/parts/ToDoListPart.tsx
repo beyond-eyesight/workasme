e18d13d3f530dd5b3729d1e52deb73c4698be625
junk-sound <wom2277@naver.com>
2021-07-03 17:12:43 +0900

    before apply dropdown

--- src/pages/management/sections/parts/ToDoListPart.tsx
+++ src/pages/management/sections/parts/ToDoListPart.tsx
@@ -3,15 +3,42 @@ import Pixel from "src/graphic/size/pixel";
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
+import {Table} from "react-bootstrap";
 
 const ToDoListPart: React.FC<{marginVertical: Pixel}> = (props: {marginVertical: Pixel}) => {
   const {marginVertical} = props;
   return <div css={css({
-    backgroundColor: 'green',
     marginTop: marginVertical.value,
     marginBottom: marginVertical.value
   })}>
 
+    <Table striped bordered hover>
+      <thead>
+      <tr>
+        <th>Task Name</th>
+        <th>Importance Level</th>
+        <th>BottleNeck</th>
+        <th>Make Priority</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>TaskName1</td>
+        <td>HIGH</td>
+        <td>SELF</td>
+      </tr>
+      <tr>
+        <td>TaskName2</td>
+        <td>HIGH</td>
+        <td>None</td>
+      </tr>
+      <tr>
+        <td>TaskName3</td>
+        <td>HIGH</td>
+        <td>contact with other team</td>
+      </tr>
+      </tbody>
+    </Table>
   </div>
 };
 export default ToDoListPart;

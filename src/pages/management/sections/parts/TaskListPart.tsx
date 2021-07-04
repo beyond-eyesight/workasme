ba203e880981942149be8e4d8d79bb773d59072e
junk-sound <wom2277@naver.com>
2021-07-04 15:05:58 +0900

    걍 코드 따라함

--- src/pages/management/sections/parts/TaskListPart.tsx
+++ src/pages/management/sections/parts/TaskListPart.tsx
@@ -1,10 +1,54 @@
-import React, {useState} from "react";
+import React, {KeyboardEventHandler, useState} from "react";
 import Pixel from "src/graphic/size/pixel";
 /** @jsxRuntime classic */
 /** @jsx jsx */
 import {css, jsx} from "@emotion/react";
 import {Button, Dropdown, Table} from "react-bootstrap";
 import {TaskListRowDto} from "src/pages/management/sections/parts/dtos/TaskListRowDto";
+import makeData from "src/pages/management/sections/parts/makedata";
+
+const Cell: React.FC<{initialValue: string}> = (props: {initialValue: string}) => {
+  const {initialValue} = props;
+  const [value, setValue] = React.useState(initialValue);
+  const [skipPageReset, setSkipPageReset] = React.useState(false);
+  const [data, setData] = React.useState(() => makeData(20))
+
+  const updateMyData = (rowIndex: number, columnId: number, value: string) => {
+    // We also turn on the flag to not reset the page
+    setSkipPageReset(true)
+    setData((old: any) => {
+        return old.map((row: any, index: number) => {
+          if (index === rowIndex) {
+            return {
+              ...old[rowIndex],
+              [columnId]: value,
+            }
+          }
+          return row
+        });
+      }
+    )
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {setValue(e.target.value)};
+  const onBlur = (e: React.ChangeEvent<HTMLInputElement>) => {updateMyData()};
+
+  function handleEnter(e: KeyboardEvent) {
+    if (e.key === "Enter") {
+      console.log("clicked enter!!")
+    }
+  }
+
+
+
+  // todo: 무슨 이벤트인지 보
+  const handleKeyPress = (e: any) => {
+    if (e.key === "Enter") {
+    }
+  };
+
+  return <input value={value} onKeyPress={handleKeyPress} onChange={onChange} onBlur={onBlur}/>
+};
 
 const TaskListPart: React.FC<{ marginVertical: Pixel }> = (props: { marginVertical: Pixel }) => {
   const {marginVertical} = props;
@@ -54,10 +98,10 @@ const TaskListPart: React.FC<{ marginVertical: Pixel }> = (props: { marginVertic
       <tbody>
       {rows.map((row) => {
         return <tr>
-          <td onClick={row.onClick}>{row.name}</td>
-          <td onClick={row.onClick}>{row.importanceLevel}</td>
-          <td onClick={row.onClick}>{row.stuckOn}</td>
-          <td onClick={row.onClick}>{row.checkPriority}</td>
+          <td onClick={row.onClick}><Cell initialValue={row.name}/></td>
+          <td onClick={row.onClick}><Cell initialValue={row.importanceLevel} /></td>
+          <td onClick={row.onClick}><Cell initialValue={row.stuckOn}/></td>
+          <td onClick={row.onClick}><Cell initialValue={row.checkPriority}/></td>
         </tr>
       })}
       </tbody>

09e1e5175d75a5739c3f54b6579e30df218a5062
junk-sound <wom2277@naver.com>
2021-07-09 20:47:41 +0900

    고군분투하여 onChange일 때 이벤트 디스패치하는거까지는 됨

--- src/pages/management/sections/parts/components/table/BasicInputCell.tsx
+++ src/pages/management/sections/parts/components/table/BasicInputCell.tsx
@@ -1,28 +1,16 @@
 import React from "react";
 import {TimeCategory, TimeSnippet} from "src/pages/management/sections/parts/dtos/TimeSnippet";
+import {useDispatch} from "react-redux";
+import { addTime } from "src/context/timeSlice";
 
 const BasicInputCell: React.FC<{ initialValue: string, isUpdating: boolean, timeSnippets: TimeSnippet[]  }> = (props: { initialValue: string, isUpdating: boolean, timeSnippets: TimeSnippet[]}) => {
   const {initialValue, isUpdating, timeSnippets} = props;
   const inputRef = React.useRef<HTMLInputElement>(null);
   const [value, setValue] = React.useState(initialValue);
 
+  const dispatch = useDispatch();
+
   console.log("BasicInputCell");
-  // @ts-ignore;
-
-  if (inputRef.current !== null) {
-    timeSnippets.push({
-      // @ts-ignore
-      expectedActivity: inputRef.current.value,
-      expectedTime: "2 hour",
-      // @ts-ignore
-      acutualActivity: inputRef.current.value,
-      actuaTime: "2 hour",
-      timeCategory: TimeCategory.ETC
-    });
-  }
-
-  console.log(timeSnippets.length);
-
 
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -35,6 +23,9 @@ const BasicInputCell: React.FC<{ initialValue: string, isUpdating: boolean, time
       inputRef.current.blur()
     }
 
+
+
+
     function isEnterPressed() {
       return e.key === "Enter";
     }
@@ -44,7 +35,7 @@ const BasicInputCell: React.FC<{ initialValue: string, isUpdating: boolean, time
     return !isUpdating;
   }
 
-  return <input disabled={isNotUpdating()} value={value} onKeyPress={handleOnlyEnterKeyPressed} onChange={onChange} ref={inputRef}/>
+  return <input onInput={() => { console.log("인풋됨.")}} disabled={isNotUpdating()} value={value} onKeyPress={handleOnlyEnterKeyPressed} onChange={onChange} ref={inputRef}/>
 };
 
 export default BasicInputCell;
